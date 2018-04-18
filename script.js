159c7774eabe367779dc001c812a2d53ea653705
Nickfas93 <nicolas.fas93@gmail.com>
2018-04-18 11:50:46 -0300

    uniao do nav com efeito, esta comentado

---
+++ script.js
@@ -0,0 +1,54 @@
+// let inputUsername = document.querySelector ("#username");
+// let inputSenha = document.querySelector ("#senha");
+// let body = document.querySelector ("body");
+
+// let logar = document.querySelector ("#login");
+// let usuario = "usuario";
+// let password = "1234";
+
+// logar.onclick = function (){
+// let username = inputUsername.value;
+// let senha = inputSenha.value;
+// if (username == usuario && senha == password) {
+//     body.style.backgroundImage = "url(https://media.giphy.com/media/fAykJdJ6SYSYw/giphy.gif)"
+
+// }
+
+
+// }
+
+
+// window.location.href = "https://media0.giphy.com/media/26ufdipQqU2lhNA4g/giphy.gif";
+
+
+// : ["nickfas93", "cristianevaz", "Larissalyra", "Vanda"],
+//     senha: ["nick21", "12345", "54321", "32145"]
+// }
+
+
+
+// while (login !=
+
+// function validarSenha(nickfas93, cristianevaz)
+// {
+//     var senha1 = document.getElementById(name1).value;
+//     var senha2 = document.getElementById(name2).value;
+		
+//     if (senha1 != "nick21" && senha2 != "12345" && senha1 === senha2)
+//     {
+//     	alert('senha iguais');
+//     }
+//     else
+//     {
+//       	alert('senhas diferentes');
+//     }
+// }
+
+// let botao = document.querySelector (".logo");
+// let nav = document.querySelector ("nav");
+// let itensLista = document.querySelectorAll ('.dropbtn');
+
+// botao.onclick = function () {
+//     nav.style.transform = "translateY(100px)";
+   
+//     }
